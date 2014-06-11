c00b14f864581bdabc623437cb2f55dbe7a4ce7e
b3ko <berkowitz.jon@gmail.com>
2014-06-11 14:13:30 -0400

    adding register page and changes to text message script

---
+++ register.php
@@ -0,0 +1,87 @@
+ <?php
+ echo <<<_END
+	<html>
+	<head>
+		<title>register - weather alert</title>
+	</head>
+	<body>
+		<div class="container">
+			<form method="POST" class="form-horizontal" role="form" method="register.php">
+			
+			<div class="form-group">
+				<label for="email">Email address:</label>
+				<input type="email" class="form-control" name="email" placeholder="you@email.com">
+			</div>
+			<div class="form-group">	
+				<label for="carrier">Mobile carrier:</label>
+				<select class="form-control" name="carrier">
+					<option value="" disabled selected>Select your carrier</option>
+					<option value="att">AT&T</option>
+					<option value="Verizon">Verizon</option>
+					<option value="Sprint">Sprint</option>
+					<option value="TMobile">T-Mobile</option>
+				</select>
+			</div>
+			<div class="form-group">
+				<label for="mobile">Mobile Phone Number:</label>
+				<input type="text" class="form-control" name="mobile" placeholder="555-867-5309">
+			</div>
+			<div class="form-group">
+				<label for="zip">Zipcode:</label>
+				<input type="text" class="form-control" name="zip" placeholder="01234">
+			</div>
+
+			<div class="form-group">
+				<input type="submit" class="btn btn-primary btn-block"/>
+			</div>
+			
+		</div>
+_END;
+	require_once 'config/db_login.php';
+	require 'header.php';
+	$db_server = mysql_connect($db_hostname, $db_username, $db_password);
+	
+	if(!$db_server) die("unable to connect to mySql: " . mysql_error());
+
+	mysql_select_db($db_database)
+		or die("unable to select datebase: " . mysql_error());
+	
+
+	if (isset($_POST['email']))
+	{
+		$email = sanitizeString($_POST['email']);
+		$zip = sanitizeString($_POST['zip']);
+		$mobile = sanitizeString($_POST['mobile']);
+		$now = date('Y-m-d H:i:s');
+		$carrier = sanitizeString($_POST['carrier']);
+		$result = mysql_query("select carrier_id from carrier_lkp where name = '$carrier'");
+		$carrier_id = mysql_result($result,0);
+		
+		$userExists = mysql_query("select mobile, user_id from users where mobile = '$mobile'");
+		if(mysql_num_rows($userExists))
+		{
+			$user_id = mysql_result($userExists,0,"user_id");
+			mysql_query("INSERT INTO zipsForUser (user_id, zipcode,isActive,querytime) VALUES('$user_id', '$zip', 1, 400)");
+		}
+		else
+		{
+			mysql_query("INSERT INTO users (email, addedDate, updateDate, mobile, carrier_id) VALUES('$email', '$now', '$now', '$mobile', '$carrier_id')");
+			mysql_query("INSERT INTO zipsForUser (user_id, zipcode,isActive) VALUES(LAST_INSERT_ID(), '$zip', 1)");	
+		}
+	}
+ 
+	mysql_close($db_server);
+	
+	function sanitizeString($var)
+	{
+		$var = strip_tags($var);
+		$var = htmlentities($var);
+		$var = stripslashes($var);
+		return mysql_real_escape_string($var);
+	}
+	
+echo <<<_END
+	</body>
+	</html>
+_END;
+ ?>
--- test.py
+++ test.py
@@ -4,26 +4,49 @@ import smtplib
 from email.mime.text import MIMEText
 import urllib2
 import json
+import MySQLdb
+import MySQLdb.cursors
+import sys
 
-recipients = config.users
-# get the data
-f = urllib2.urlopen('http://api.wunderground.com/api/%s/forecast/q/PA/pittsburgh.json' % config.key)
-json_string = f.read()
-parsed_json = json.loads(json_string)
-high = parsed_json['forecast']['simpleforecast']['forecastday'][0]['high']['fahrenheit']
-low = parsed_json['forecast']['simpleforecast']['forecastday'][0]['low']['fahrenheit']
-conditions = parsed_json['forecast']['simpleforecast']['forecastday'][0]['conditions']
-day = parsed_json['forecast']['simpleforecast']['forecastday'][0]['date']['weekday_short']
-f.close()
+db = MySQLdb.connect (host = config.host, user = config.user, passwd = config.passwd, db = config.db, cursorclass=MySQLdb.cursors.DictCursor)
+cursor = db.cursor()
+#group by zipcode to reduce api calls (one per zip)
+cursor.execute("select zipcode from zipsForUser group by zipcode")
+zips = []
+rows = cursor.fetchall()
+for row in rows :
+	zips.append(row['zipcode'])
+	
+# get the weather per zip
+for zip in zips :
+	f = urllib2.urlopen('http://api.wunderground.com/api/%s/geolookup/forecast/q/%s.json' % (config.key, zip))
+	json_string = f.read()
+	parsed_json = json.loads(json_string)
+	high = parsed_json['forecast']['simpleforecast']['forecastday'][0]['high']['fahrenheit']
+	low = parsed_json['forecast']['simpleforecast']['forecastday'][0]['low']['fahrenheit']
+	conditions = parsed_json['forecast']['simpleforecast']['forecastday'][0]['conditions']
+	day = parsed_json['forecast']['simpleforecast']['forecastday'][0]['date']['weekday_short']
+	location = parsed_json['location']['city']
+	f.close()
+# get users for this zip
+	cursor.execute("select concat(u.mobile, '@', cl.domain) as number from carrier_lkp cl \
+		left outer join users u on u.carrier_id = cl.carrier_id \
+		left outer join zipsForUser z on z.user_id = u.user_id \
+		where z.zipcode = %s" % zip)
+	recipients = []
+	rows = cursor.fetchall()
+	for row in rows :
+		recipients.append(row['number'])
 
-# set the message's attributes
-msg = MIMEText("Forecast: %s, High: %s, Low: %s" % (conditions, high, low))
-msg['Subject'] = 'Pittsburgh forecast for %s' % day
-msg['From'] = 'mail@atownrobots.com'
-msg['To'] = ', '.join( recipients )
+	# set the message's attributes
+	msg = MIMEText("Forecast: %s, High: %s, Low: %s" % (conditions, high, low))
+	msg['Subject'] = '%s forecast for %s' % (location, day)
+	msg['From'] = 'mail@atownrobots.com'
+	msg['To'] = ', '.join( recipients )
 
-# send the message:
-s = smtplib.SMTP('mail.atownrobots.com', 587)
-s.login('mail@atownrobots.com', config.password)
-s.sendmail(msg['from'], recipients, msg.as_string())
-s.quit()
+	# send the message:
+	s = smtplib.SMTP('mail.atownrobots.com', 587)
+	s.login('mail@atownrobots.com', config.password)
+	s.sendmail(msg['from'], recipients, msg.as_string())
+	s.quit()
+db.close()
